d0dd73ed8891fe24fad1ddff017dbcc84f2860ce
Joseph Huckaby <jhuckaby@pixlcore.com>
2026-08-19 11:14:15 -0700

    Bug Fix: Prevent negative numbers from breaking server and group donut pies.  Fixes #419.

--- htdocs/js/pages/Servers.class.js
+++ htdocs/js/pages/Servers.class.js
@@ -1700,8 +1700,8 @@ Page.Servers = class Servers extends Page.ServerUtils {
 				var key = RegExp.$1;
 				var opts = this.donutDashUnits[id];
 				
-				var new_value = mem[key] || 0;
-				var old_value = opts.value || 0;
+				var new_value = Math.max( 0, mem[key] || 0 );
+				var old_value = Math.max( 0, opts.value || 0 );
 				
 				var new_pct = short_float( (new_value / (opts.max || 1)) * 100, 3 );
 				var old_pct = short_float( (old_value / (opts.max || 1)) * 100, 3 );
@@ -1736,8 +1736,8 @@ Page.Servers = class Servers extends Page.ServerUtils {
 				var key = RegExp.$1;
 				var opts = this.donutDashUnits[id];
 				
-				var new_value = cpu_totals[key] || 0;
-				var old_value = opts.value || 0;
+				var new_value = Math.max( 0, cpu_totals[key] || 0 );
+				var old_value = Math.max( 0, opts.value || 0 );
 				
 				var new_pct = short_float( (new_value / (opts.max || 1)) * 100, 3 );
 				var old_pct = short_float( (old_value / (opts.max || 1)) * 100, 3 );
